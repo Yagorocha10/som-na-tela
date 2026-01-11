6e3356fa27f621ddfc686b8fd6c8bfc262d34490
Yagorocha10 <yagodasilvarocha30@gmail.com>
2026-01-11 15:15:20 -0300

    Primeiro commit

---
+++ src/main/java/com/example/somnatela/SomnatelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.somnatela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomnatelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomnatelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somnatela/SomnatelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somnatela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomnatelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
